1ffb4b187c4a3b08dccee9edfab105826c62edfd
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-21 09:43:00 -0400

    Get rid of redundant steps in the setupTable function

--- main.js
+++ main.js
@@ -34,16 +34,14 @@ const drawCard = async count => {
 
 const setupTable = async () => {
   await getCardDeck();
-  let player1Pile = [];
-  let player2Pile = [];
+  let player1Pile;
+  let player2Pile;
 
-  // const p1Cards = await drawCard(5);
-  const p2Cards = await drawCard(5);
-  // player1Pile.push(p1Cards)
-  player2Pile.push(p2Cards)
-  // console.log(player1Pile)
-  console.log(player2Pile[0][0])
-  const p2Card1Div = document.querySelector('#player2-cards .card1');
-  p2Card1Div.innerHTML = `<img src='${player2Pile[0][0].image}'/>`
+  player1Pile = await drawCard(5);
+  player2Pile = await drawCard(5);
+  console.log({player1Pile})
+  console.log({player2Pile})
+  // const p2Card1Div = document.querySelector('#player2-cards .card1');
+  // p2Card1Div.innerHTML = `<img src='${player2Pile.image}'/>`
 }
-// setupTable()
+// setupTable();
