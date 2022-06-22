96ae8ee65ad3db5ef0b7fb0482d5febf0608dc7f
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-22 15:01:09 -0400

    Create draw and discard piles, and add some styling

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ dotenv.config();
 
 const BASE_URL = process.env.BASE_URL;
 let deckId;
-const player1Cards = document.querySelectorAll('#player1-cards .card');
-const player2Cards = document.querySelectorAll('#player2-cards .card');
+let player1Cards = document.querySelectorAll('#player1-cards .card');
+player1Cards = Array.from(player1Cards);
+let player2Cards = document.querySelectorAll('#player2-cards .card');
+player2Cards = Array.from(player2Cards);
+const pickupPile = document.querySelector('#pickup-pile');
+const discardPile = document.querySelector('#discard-btn');
+let currentClickedCardIndex;
 
  const getCardDeck = async () => {
   try {
@@ -38,15 +43,37 @@ const setupTable = async () => {
   await getCardDeck();
   let player1Pile;
   let player2Pile;
+  let drawPile;
 
   player1Pile = await drawCard(5);
   player2Pile = await drawCard(5);
-  console.log({player1Pile})
-  console.log({player2Pile})
+  // drawPile = await axios.get(`${BASE_URL}${deckId}/`);
+  // console.log(drawPile)
+  // console.log({player1Pile})
+  // console.log({player2Pile})
 
+  // drawPile.addEventListener('click', );
   for(let i = 0; i < 5; i++) {
-    player1Cards[i].innerHTML = `<img src='${player1Pile[i].image}'/>`;
-    player2Cards[i].innerHTML = `<img src='${player2Pile[i].image}'/>`;
+    player1Cards[i].innerHTML = `<img src='${player1Pile[i].image}' alt=''/>`;
+    
+    player2Cards[i].innerHTML = `<img src='${player2Pile[i].image}' alt=''/>`;
   }
+  return [player1Pile, player2Pile];
 }
-setupTable();
+// setupTable();
+
+const playGame = async () => {
+  // await setupTable()
+  // console.log(currentClickedCardIndex)
+  let [player1Pile, player2Pile] = await setupTable();
+  
+  player1Cards.forEach((card,i) => {
+   card.addEventListener('click', () => {currentClickedCardIndex = i; console.log(player1Pile[currentClickedCardIndex])})
+  })
+  player2Cards.forEach((card,i) => {
+   card.addEventListener('click', () => {currentClickedCardIndex = i; console.log(currentClickedCardIndex)})
+  })
+   
+  
+}
+playGame()
