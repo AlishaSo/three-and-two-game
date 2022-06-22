ba6181831e9011df55b48d8897aaab5bf6062832
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-22 16:10:12 -0400

    Make the pickup pile image flip over when the user clicks on it, and flip back when they've chose a card to swap it with

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
+import img from './card-back-black.png';
 
 const BASE_URL = process.env.BASE_URL;
 let deckId;
@@ -43,16 +44,12 @@ const setupTable = async () => {
   await getCardDeck();
   let player1Pile;
   let player2Pile;
-  let drawPile;
 
   player1Pile = await drawCard(5);
   player2Pile = await drawCard(5);
-  // drawPile = await axios.get(`${BASE_URL}${deckId}/`);
-  // console.log(drawPile)
   // console.log({player1Pile})
   // console.log({player2Pile})
 
-  // drawPile.addEventListener('click', );
   for(let i = 0; i < 5; i++) {
     player1Cards[i].innerHTML = `<img src='${player1Pile[i].image}' alt=''/>`;
     
@@ -66,14 +63,31 @@ const playGame = async () => {
   // await setupTable()
   // console.log(currentClickedCardIndex)
   let [player1Pile, player2Pile] = await setupTable();
-  
+  // let drawPile;
+  let pickedUpCard;
+  const pickupPileImg = document.querySelector('#pickup-pile-img')
+
   player1Cards.forEach((card,i) => {
-   card.addEventListener('click', () => {currentClickedCardIndex = i; console.log(player1Pile[currentClickedCardIndex])})
+   card.addEventListener('click', () => {
+    currentClickedCardIndex = i; 
+    console.log(player1Pile[currentClickedCardIndex])
+    player1Pile[currentClickedCardIndex] = pickedUpCard;
+    card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
+    pickupPileImg.src = img;
+    })
   })
   player2Cards.forEach((card,i) => {
    card.addEventListener('click', () => {currentClickedCardIndex = i; console.log(currentClickedCardIndex)})
   })
    
+  pickupPile.addEventListener('click', async () => {
+    console.log('pickup clicked')
+    // drawPile = await axios.get(`${BASE_URL}${deckId}/`);
+    pickedUpCard = await drawCard(1);
+    // console.log(pickedUpCard)
+    // pickupPileImg.classList.add('card');
+    pickupPileImg.setAttribute('src', pickedUpCard[0].image);
+  });
   
 }
 playGame()
