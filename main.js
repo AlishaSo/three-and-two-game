5d3a78ad94dac12d05dc0238b3877dba0cad793a
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-23 16:54:56 -0400

    Make the player's card unclickable when it's not their turn

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ const BASE_URL = process.env.BASE_URL;
 let deckId;
 let player1Cards = document.querySelectorAll('#player1-cards .card');
 player1Cards = Array.from(player1Cards);
-let player2Cards = document.querySelectorAll('#player2-cards .card');
-player2Cards = Array.from(player2Cards);
+let computersCards = document.querySelectorAll('#player2-cards .card');
+computersCards = Array.from(computersCards);
 const pickupPile = document.querySelector('#pickup-pile');
 const discardPile = document.querySelector('#discard-btn');
 let discardList;
 const pickupPileImg = document.querySelector('#pickup-pile-img');
 let player1Pile;
-let player2Pile;
-let pickedUpCard;
+let computersPile;
+let pickedUpCard = null;
 let usersTurn = true;
 
  const getCardDeck = async () => {
@@ -48,38 +48,21 @@ const drawCard = async count => {
 const setupTable = async () => {
   await getCardDeck();
   let player1Pile;
-  let player2Pile;
+  let computersPile;
 
   player1Pile = await drawCard(5);
-  player2Pile = await drawCard(5);
+  computersPile = await drawCard(5);
   // console.log({player1Pile})
   // console.log({player2Pile})
 
   for(let i = 0; i < 5; i++) {
     player1Cards[i].innerHTML = `<img src='${player1Pile[i].image}' alt=''/>`;
     
-    player2Cards[i].innerHTML = `<img src='${player2Pile[i].image}' alt=''/>`;
+    computersCards[i].innerHTML = `<img src='${computersPile[i].image}' alt=''/>`;
   }
-  return [player1Pile, player2Pile];
+  return [player1Pile, computersPile];
 }
 // setupTable();
-
-// const do1 = (card, i) => {
-//   if(usersTurn) {
-//     player1Pile[i] = pickedUpCard;
-//     card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
-//     pickupPileImg.src = img;
-//   pickedUpCard = '';
-//   }
-// }
-
-// function do2(card, i, pickedUpCard) {
-//   if(!usersTurn) {
-//     player2Pile[i] = pickedUpCard;
-//     card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
-//     pickupPileImg.src = img;
-//   }
-// }
 
 pickupPile.onclick = async () => {
     pickedUpCard = await drawCard(1);
@@ -90,57 +73,36 @@ pickupPile.onclick = async () => {
 const playGame = async () => {
   let [ p1,  p2] = await setupTable();
   player1Pile = p1;
-  player2Pile = p2;
+  computersPile = p2;
   let tempBool = true;
 
   let i = 0;
   while(tempBool) {
    i++; console.log({i, usersTurn})
-  // pickupPile.addEventListener('click', async () => {
-  //   pickedUpCard = await drawCard(1);
-  //   // console.log(pickedUpCard)
-  //   pickupPileImg.setAttribute('src', pickedUpCard[0].image);
-  // });
   
   discardPile.addEventListener('click', async () => {
     await axios.get(`${BASE_URL}/${deckId}/pile/discard/add/?cards=${pickedUpCard[0].code}`);
     pickupPileImg.src = img;
   });
 
-  // if(usersTurn) {
-    // player2Cards.forEach((card,i) => {
-    //   card.removeEventListener('click', () => {
-    //    player2Pile[i] = pickedUpCard;
-    //    card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
-    //    pickupPileImg.src = img;
-    //   });
-    //  });
-
+  if(usersTurn) {
   player1Cards.forEach((card,i) => {
-   card.onclick = () => {
-    if(usersTurn) {
-      player1Pile[i] = pickedUpCard;
-      card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
-      pickupPileImg.src = img;
-    pickedUpCard = undefined;
-    }
-  };
+    card.onclick = () => {
+      if(usersTurn) {
+        if(pickedUpCard != null){
+          player1Pile[i] = pickedUpCard;
+          card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
+          pickupPileImg.src = img;
+          pickedUpCard = null;
+        }
+      }
+    };
   });
-  // }
+  }
   
-  // else {
-    // player1Cards.forEach((card,i) => {
-    //   card.removeEventListener('click', () => {
-    //    player1Pile[i] = pickedUpCard;
-    //    card.innerHTML = `<img src='${pickedUpCard[0].image}' alt=''/>`;
-    //    pickupPileImg.src = img;
-    //    });
-    //  });
+  else {
 
-  // player2Cards.forEach((card,i) => {
-  //  card.addEventListener('click', () => do2(player2Pile, card, i, pickedUpCard));
-  // });
-  // }
+  }
   usersTurn = !usersTurn;
   if(i == 2)
     tempBool = false;
