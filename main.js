3e4ef06669ef90b06a316f703bb6fb3a64e6d078
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-23 19:40:56 -0400

    Add the functionality for the discard pile to behave the way it's meant to; remove unneeded css rules

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let discardList;
 const pickupPileImg = document.querySelector('#pickup-pile-img');
 let player1Pile;
 let computersPile;
-let discardCard;
+let discardCard = [];
 let pickedUpCard = null;
 let usersTurn = true;
 
@@ -25,7 +25,6 @@ let usersTurn = true;
     // console.log(info)
     deckId = await info.data.deck_id;
     // console.log(deckId)
-    // return info.data.deck_id;
   } catch(e) {
     console.error(e);
   }
@@ -48,33 +47,30 @@ const drawCard = async count => {
 
 const setupTable = async () => {
   await getCardDeck();
-  // let player1Pile;
-  // let computersPile;
 
   player1Pile = await drawCard(5);
   computersPile = await drawCard(5);
-  discardCard = await drawCard(1);
+  discardCard.push(await drawCard(1));
 
-  discardPile.innerHTML = `<img src='${discardCard[0].image}' alt=''/>`;
+  discardPile.innerHTML = `<img src='${discardCard[0][0].image}' alt=''/>`;
+
   for(let i = 0; i < 5; i++) {
     player1Cards[i].innerHTML = `<img src='${player1Pile[i].image}' alt=''/>`;
     
     computersCards[i].innerHTML = `<img src='${computersPile[i].image}' alt=''/>`;
   }
-  // return [player1Pile, computersPile];
 }
 // setupTable();
 
 pickupPile.onclick = async () => {
+  if(usersTurn) {
     pickedUpCard = await drawCard(1);
     // console.log(pickedUpCard)
     pickupPileImg.setAttribute('src', pickedUpCard[0].image);
+  }
 }
 
 const playGame = async () => {
-  // let [ p1,  p2] = await setupTable();
-  // player1Pile = p1;
-  // computersPile = p2;
   await setupTable();
   let tempBool = true;
 
@@ -82,10 +78,21 @@ const playGame = async () => {
   while(tempBool) {
    i++; console.log({i, usersTurn})
   
-  discardPile.addEventListener('click', async () => {
-    await axios.get(`${BASE_URL}/${deckId}/pile/discard/add/?cards=${pickedUpCard[0].code}`);
-    pickupPileImg.src = img;
-  });
+  discardPile.onclick = async () => {
+    if(usersTurn) {
+      if(pickedUpCard != null) {
+        discardCard[0].push(pickedUpCard[0]);
+        await axios.get(`${BASE_URL}/${deckId}/pile/discard/add/?cards=${pickedUpCard[0].code}`);
+          console.log(discardCard[0])
+        discardPile.innerHTML = `<img src='${discardCard[0][1].image}' alt=''/>`;
+        pickedUpCard = null;
+        pickupPileImg.src = img;
+      }
+      else {
+        await axios.get(`${BASE_URL}/${deckId}/pile/discard/add/?cards=${discardCard[0][0].code}`);
+      }
+    }
+  };
 
   if(usersTurn) {
     player1Cards.forEach((card,i) => {
@@ -97,14 +104,30 @@ const playGame = async () => {
             pickupPileImg.src = img;
             pickedUpCard = null;
           }
+          else {
+            if(discardCard.length == 2) {
+              player1Pile[i] = discardCard[0][1];
+              card.innerHTML = `<img src='${discardCard[0][1].image}' alt=''/>`;
+              discardCard.pop();
+              discardPile.innerHTML = `<img src='${discardCard[0][0].image}' alt=''`;
+            }
+            if(discardCard.length == 1) {
+              discardCard[0].push(player1Pile[i]);
+              player1Pile[i] = discardCard[0][0];
+              card.innerHTML = `<img src='${discardCard[0][0].image}' alt=''/>`;
+              discardPile.innerHTML = `<img src='${discardCard[0][1].image}' alt=''/>`;
+              discardCard[0].shift();
+            }
+          }
+          
         }
-      };
+      }
     });
   }
   
-  else {
+  // else {
 
-  }
+  // }
   usersTurn = !usersTurn;
   if(i == 2)
     tempBool = false;
